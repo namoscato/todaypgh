c5c2ac732d5558aebb78529265aae21dfc8543e0
Nick Amoscato <nick@amoscato.com>
2020-05-17 15:40:42 -0400

    Implement referrals (#6)
    Also updates header styles and centers bridge

---
+++ assets/script/spread/index.js
@@ -0,0 +1,80 @@
+/* global $, btoa, ClipboardJS, ga */
+(function () {
+  const form = $('#spread-form')
+  const link = $('#spread-link')
+  const linkBack = $('#spread-link-back')
+  const linkClipboard = new ClipboardJS('#spread-link-button')
+  const linkInput = $('#spread-link-input')
+  const shareUrl = form.data('share-url')
+  const shareUrlEncoded = encodeURIComponent(shareUrl)
+  const socialLinks = $('[data-share-social]')
+
+  let referrerEmail
+  let referrerName
+
+  form.submit(function (event) {
+    event.preventDefault()
+
+    referrerEmail = $('#email').val()
+    referrerName = $('#name').val()
+
+    linkInput.val(getReferrerUrl('link'))
+
+    socialLinks.each(function () {
+      const me = $(this)
+
+      me.attr(
+        'href',
+        me.attr('href').replace(shareUrlEncoded, encodeURIComponent(getReferrerUrl(me.data('share-social'))))
+      )
+    })
+
+    link.show()
+    form.hide()
+
+    trackEvent('Submit', referrerEmail)
+  })
+
+  linkBack.click(function (event) {
+    event.preventDefault()
+
+    link.hide()
+    form.show()
+
+    trackEvent('Back')
+  })
+
+  linkInput.click(function () {
+    linkInput.select()
+
+    trackEvent('Share', 'select')
+  })
+
+  linkClipboard.on('success', function (event) {
+    const linkButton = event.trigger
+
+    linkButton.innerText = 'Copied link'
+
+    setTimeout(function () {
+      linkButton.innerText = 'Copy link'
+    }, 2000)
+
+    trackEvent('Share', 'copy')
+  })
+
+  socialLinks.click(function () {
+    trackEvent('Share', $(this).data('share-social'))
+  })
+
+  function getReferrerUrl (medium) {
+    return shareUrl + '?r=' + encodeURIComponent(btoa(JSON.stringify({
+      email: referrerEmail,
+      name: referrerName,
+      medium: medium
+    })))
+  }
+
+  function trackEvent (action, label) {
+    ga('send', 'event', 'Spread', action, label)
+  }
+})()
---
+++ content/spread-the-love/share.md
@@ -0,0 +1,11 @@
+---
+title: Share Your Story
+---
+
+_Dear educator…_
+
+Thank you! [TodayPGH]({{< ref "/" >}}) wants you to know that the difference you make is immeasurable.
+
+{{< id text-referrer-name >}}A student{{< /id >}} invites you to share your story:
+
+{{< share-your-story >}}
